5efa66c70e71016a8e28e4ce8f5ade1761669e80
Vinicius Mussato <vmussato.vermont@vstsalelo.onmicrosoft.com>
2020-09-20 21:17:29 -0300

    created components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,17 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserItemComponent } from './user/user-item/user-item.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    UserListComponent,
+    UserItemComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
